44fa7db4d08646edcef1c022ffc23373b08b062b
suresh <sureshvoltech3257@gmail.com>
2021-03-27 17:15:36 +0530

    newfolder

---
+++ maven-project/src/main/java/maven/business/CategoryBC.java
@@ -0,0 +1,13 @@
+package maven.business;
+
+import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+import br.gov.frameworkdemoiselle.template.DelegateCrud;
+import maven.domain.Category;
+import maven.persistence.CategoryDAO;
+
+@BusinessController
+public class CategoryBC extends DelegateCrud<Category, Long, CategoryDAO> {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ maven-project/src/main/java/maven/persistence/CategoryDAO.java
@@ -0,0 +1,12 @@
+package maven.persistence;
+
+import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
+import br.gov.frameworkdemoiselle.template.JPACrud;
+import maven.domain.Category;
+
+@PersistenceController
+public class CategoryDAO extends JPACrud<Category, Long> {
+
+	private static final long serialVersionUID = 1L;
+
+}
